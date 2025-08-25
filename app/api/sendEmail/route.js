e21a9683a7fa34175da474a1a4250c1e4f98b422
TarunpreetSingh23 <tarunpreets29@gmail.com>
2025-08-25 13:10:13 +0530

    Update route.js

--- app/api/sendEmail/route.js
+++ app/api/sendEmail/route.js
@@ -1,9 +1,1 @@
-import { sendEmail } from "@/helper/mailer";
-import { NextResponse } from "next/server";
 
-export async function POST(req) {
-  const { to, subject, text } = await req.json();
-  await sendEmail({ to, subject, text });
-
-  return NextResponse.json({ message: "Email sent successfully!" });
-}
